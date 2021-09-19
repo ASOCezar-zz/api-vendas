e9a7e9604b567c371d5422b5586b222f05473b34
asocezar <aso.cezar00@gmail.com>
2021-09-19 17:23:37 -0300

    first commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,33 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 500,
+      message: 'Internal Server Error',
+    });
+  },
+);
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}! 🎉`);
+});
