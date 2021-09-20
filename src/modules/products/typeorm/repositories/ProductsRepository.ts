6dff87893e9e917788034ded0d2bbdcfb55f3f4e
asocezar <aso.cezar00@gmail.com>
2021-09-20 15:45:48 -0300

    First custom repository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Product from '../entities/Product';
+
+@EntityRepository(Product)
+export class ProductsRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    return this.findOne({
+      where: { name },
+    });
+  }
+}
