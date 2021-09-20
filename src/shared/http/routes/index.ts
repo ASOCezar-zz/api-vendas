66d9074a6e27fc5e4fb5d20512eb85354c99b888
asocezar <aso.cezar00@gmail.com>
2021-09-20 17:28:33 -0300

    Products routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
+
+routes.use('/products', productsRouter);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello Dev!' });
