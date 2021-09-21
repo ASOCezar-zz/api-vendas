420211072b4e12ab6dc2bc696eeff0b2a8fd1d57
asocezar <aso.cezar00@gmail.com>
2021-09-20 23:12:26 -0300

    Sessions Controller

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import CreateSessionService from '../services/CreateSessionsService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionService();
+
+    const session = await createSession.execute({ email, password });
+
+    return response.json(session);
+  }
+}
