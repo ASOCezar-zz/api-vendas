49a04f08d9cc42c74bbcdad64a5c845af1ab5ba8
asocezar <aso.cezar00@gmail.com>
2021-09-21 13:34:54 -0300

    Reset Password Controller

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,14 @@
+import { Request } from 'express';
+import ResetUserPasswordService from '../services/ResetUserPasswordService';
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { token, password } = request.body;
+
+    const resetUserPassword = new ResetUserPasswordService();
+
+    await resetUserPassword.execute({ token, password });
+
+    return response.status(204).json();
+  }
+}
