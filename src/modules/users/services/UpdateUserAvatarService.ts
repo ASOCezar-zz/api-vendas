0728150879d049269977277e6fb13375cca4288e
asocezar <aso.cezar00@gmail.com>
2021-09-21 11:10:38 -0300

    Update and Upload User Avatar Service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+import path from 'path';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+type UpdateUserAvatarType = {
+  user_id: string;
+  avatarFilename: string;
+};
+
+export default class UpdateUserAvatarService {
+  public async execute({
+    user_id,
+    avatarFilename,
+  }: UpdateUserAvatarType): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User Not Found', 404);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
