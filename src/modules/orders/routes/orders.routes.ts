56877188559f5665fc87c8a9966648bebf66faa9
asocezar <aso.cezar00@gmail.com>
2021-09-22 19:40:54 -0300

    Orders Router

---
+++ src/modules/orders/routes/orders.routes.ts
@@ -0,0 +1,31 @@
+import isAuthenticated from '@shared/http/middlewares/ isAuthenticated';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import OrdersController from '../controllers/OrdersController';
+
+const ordersRouter = Router();
+const ordersController = new OrdersController();
+
+ordersRouter.use(isAuthenticated);
+
+ordersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  ordersController.show,
+);
+ordersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      customer_id: Joi.string().uuid().required(),
+      products: Joi.array().required(),
+    },
+  }),
+  ordersController.create,
+);
+
+export default ordersRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,6 +5,7 @@ import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customerRouter from '@modules/customers/routes/customer.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -14,6 +15,7 @@ routes.use('/products', productsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customerRouter);
+routes.use('/orders', ordersRouter);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello Dev!' });
